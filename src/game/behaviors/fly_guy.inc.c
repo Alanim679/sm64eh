632d6ae82b579b164d753475efd3128ff8811331
Alanim679 <Prophetzeus@gmail.com>
2024-11-15 01:16:44 -0500

    Update fly_guy.inc.c

--- src/game/behaviors/fly_guy.inc.c
+++ src/game/behaviors/fly_guy.inc.c
@@ -9,7 +9,7 @@
 static struct ObjectHitbox sFlyGuyHitbox = {
     /* interactType:      */ INTERACT_BOUNCE_TOP,
     /* downOffset:        */ 0,
-    /* damageOrCoinValue: */ 2,
+    /* damageOrCoinValue: */ 8,
     /* health:            */ 0,
     /* numLootCoins:      */ 2,
     /* radius:            */ 70,
