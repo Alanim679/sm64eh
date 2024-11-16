0c7f58170dd273e4cca784ad11a75e211d77c8b4
Alanim679 <Prophetzeus@gmail.com>
2024-11-16 01:13:08 -0500

    Update behavior_data.c

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -378,7 +378,7 @@ const BehaviorScript bhvMrIParticle[] = {
     OR_INT(oFlags, (OBJ_FLAG_MOVE_XZ_USING_FVEL | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
     SET_INT(oIntangibleTimer, 0),
     SET_HITBOX(50, 50),
-    SET_INT(oDamageOrCoinValue, 1),
+    SET_INT(oDamageOrCoinValue, 8),
     SET_INT(oInteractType, INTERACT_DAMAGE),
     SET_OBJ_PHYSICS(/*Wall hitbox radius*/ 30, /*Gravity*/ 0, /*Bounciness*/ 0, /*Drag strength*/ 0, /*Friction*/ 0, /*Buoyancy*/ 0, /*Unused*/ 0, 0),
     CALL_NATIVE(bhv_init_room),
